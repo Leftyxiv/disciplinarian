8be3868749fae03d40c1ad1486918dd7734d3a95
leftyxiv <manny@aledoux.net>
2021-12-02 18:51:44 -0500

    added a user schema

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: [true, 'Username is already taken'],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
