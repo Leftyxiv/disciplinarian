ea57f5687963eed9affe60962b14e7ece5efb32f
leftyxiv <manny@aledoux.net>
2021-12-02 18:59:09 -0500

    subtopic schema is in

---
+++ models/SubTopic.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const subTopicSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  unitOfMeasure: {
+    type: String,
+    required: true,
+  },
+  unitOfMeasureTwo: {
+    type: String,
+  }
+  parent_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Topic',
+    required: true,
+  }
+});
+
+const SubTopic = mongoose.model('SubTopic', subTopicSchema);
+module.exports = SubTopic;
