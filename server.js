c4a6c81508d3a9e7c1e5b99306a9506e9054f2b7
leftyxiv <manny@aledoux.net>
2021-12-03 10:28:53 -0500

    can render my stuff to the screen

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const topicRouter = require('./routes/topicRoutes.js');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./client/dist'));
 
 const port = 3000;
 
