fa0390d76dfc063f2990d0ac1547b450946cd06f
leftyxiv <manny@aledoux.net>
2021-12-03 11:37:27 -0500

    log in form is working

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -8,21 +8,24 @@ const LoginForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios.post('/api/users/login', { username, password }).then((res) => {
-      console.log(res);
+      window.localStorage.setItem('discAuth', res.data.user._id);
     });
   };
 
-  return <form>
-  <div className="form-group">
-    <label>Username</label>
-    <input onChange={(e) => setUsername(e.target.value)} type="text" className="form-control" placeholder="Enter Username" />
+  return <div>
+    <h1>Login</h1>
+    <form>
+      <div className="form-group">
+        <label>Username</label>
+        <input onChange={(e) => setUsername(e.target.value)} type="text" className="form-control" placeholder="Enter Username" />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input onChange={(e) => setPassword(e.target.value)} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+      </div>
+      <button onClick={handleSubmit} type="button" className="btn btn-dark">Submit</button>
+    </form>
   </div>
-  <div className="form-group">
-    <label>Password</label>
-    <input onChange={(e) => setPassword(e.target.value)} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-  </div>
-  <button onClick={handleSubmit} type="button" className="btn btn-dark">Submit</button>
-</form>
 };
 
 export default LoginForm;
