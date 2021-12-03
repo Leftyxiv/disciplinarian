7693ff6df9c821fe127f0f4bb9e60b9d9fae0da2
leftyxiv <manny@aledoux.net>
2021-12-02 19:24:34 -0500

    first user route is in and working

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,16 @@ const User = require('../models/User.js');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+const catchAsync = require('../utils/catchAsync');
+
+router.get('/', catchAsync(async (req, res) => {
   const users = await User.find({});
-});
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    users,
+  });
+}));
+
 
 module.exports = router;
