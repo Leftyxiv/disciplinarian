8b976a48d7f348396d49abfb7d0f6c5dd9176bc9
leftyxiv <manny@aledoux.net>
2021-12-02 19:29:30 -0500

    can successfully post a new user to the database

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const User = require('../models/User.js');
 
 const router = express.Router();
@@ -14,5 +15,20 @@ router.get('/', catchAsync(async (req, res) => {
   });
 }));
 
+router.post('/signup', catchAsync(async (req, res) => {
+  const password = await bcrypt.hash(req.body.password, 12);
+  req.body.password = password;
+  const newUser = await User.create(req.body);
+  if (!newUser) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'User not created',
+    });
+  }
+  return res.status(201).json({
+    status: 'success',
+    user: newUser,
+  });
+}));
 
 module.exports = router;
