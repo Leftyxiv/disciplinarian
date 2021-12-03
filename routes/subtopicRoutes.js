6aaf0ed03f9e653dc5d5a8193777a24143b81bf5
leftyxiv <manny@aledoux.net>
2021-12-03 09:43:14 -0500

    finished subtopic routes

--- routes/subtopicRoutes.js
+++ routes/subtopicRoutes.js
@@ -27,4 +27,44 @@ router.post('/', catchAsync(async (req, res, next) => {
   });
 }));
 
+router.get('/:subtopicId', catchAsync(async (req, res, next) => {
+  const subTopic = await SubTopic.findById(req.params.subtopicId);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      subTopic
+    }
+  });
+}));
+
+router.patch('/:subtopicId', catchAsync(async (req, res, next) => {
+  const subTopic = await SubTopic.findByIdAndUpdate(req.params.subtopicId, req.body);
+  if (!subTopic) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Subtopic not found'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      subTopic
+    }
+  });
+}));
+
+router.delete('/:subtopicId', catchAsync(async (req, res, next) => {
+  const subTopic = await SubTopic.findByIdAndDelete(req.params.subtopicId);
+  if (!subTopic) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Subtopic not found'
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+}));
+
 module.exports = router;
